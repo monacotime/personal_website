93f8cfc4c0840bbf9c4eab6ee1df8a5c61d57f6c
Rudramani Singha <monacotime@outlook.com>
2021-11-06 08:20:33 +0530

    hmm

---
+++ script.js
@@ -0,0 +1,28 @@
+// nextParticle = document.all.logo.nextParticle;
+// nextParticle.width = window.innerWidth;
+// nextParticle.height = window.innerHeight;
+// window.onresize = function () {
+    // console.log(document.all.logo.width)
+    // nextParticle.width = window.innerWidth;
+    // nextParticle.height = document.all.logo.height;
+//     nextParticle.start();
+// };
+
+// nextParticle = document.all.logo.nextParticle;
+// nextParticle.width = window.innerWidth;
+// nextParticle.height = window.innerHeight;
+
+// nextParticle = new NextParticle({
+//     image: document.all.logo,
+//     width: window.innerWidth,
+//     height: window.innerHeight,
+//     particleGap: 5,
+//     renderer: "webgl"
+// });
+// window.onresize = function () {
+//     if (window.innerWidth > 600) {
+//         nextParticle.width = window.innerWidth;
+//         nextParticle.height = window.innerHeight;
+//         nextParticle.start();
+//     }
+// };
